4de6878b140327c0a086d312f755537e2569c59d
jangsw12 <156777305+jangsw12@users.noreply.github.com>
2026-05-22 18:21:18 +0900

    Initial commit

---
+++ GroupWare_Client/GroupWare_Client/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using GroupWare_Client.ViewModels;
+using GroupWare_Client.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GroupWare_Client
+{
+    public partial class App : Application
+    {
+        public IServiceProvider Services { get; }
+
+        public App()
+        {
+            Services = ConfigureServices();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainView = Services.GetRequiredService<MainView>();
+            mainView.Show();
+        }
+
+        public new static App Current => (App)Application.Current;
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // ViewModels
+            services.AddSingleton<MainViewModel>();
+
+            // Views
+            services.AddSingleton(s => new MainView()
+            {
+                DataContext = s.GetRequiredService<MainViewModel>()
+            });
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
